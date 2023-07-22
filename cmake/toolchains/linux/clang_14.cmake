0513b62a7e29062d3e6820c8322358694130a235
Ruslan Golovinsky <golxzn@gmail.com>
2023-07-22 20:43:12 +0400

    Add ccache support to linux

--- cmake/toolchains/linux/clang_14.cmake
+++ cmake/toolchains/linux/clang_14.cmake
@@ -5,3 +5,9 @@ set(CMAKE_HOST_SYSTEM_PROCESSOR x86_64 CACHE STRING "Host system processor")
 
 set(CMAKE_C_COMPILER clang-15)
 set(CMAKE_CXX_COMPILER clang++-15)
+
+find_program(ccache_program ccache)
+if (NOT ${ccache_program} MATCHES "NOTFOUND")
+	set(CMAKE_C_COMPILER_LAUNCHER ccache)
+	set(CMAKE_CXX_COMPILER_LAUNCHER ccache)
+endif()
