317b46ae83662978b47c8b74803c8bba3a7016a5
Ruslan Golovinsky <golxzn@gmail.com>
2023-07-23 08:21:05 +0400

    Remove docs generating during build

--- cmake/dev-mode.cmake
+++ cmake/dev-mode.cmake
@@ -8,6 +8,6 @@ if(GRM_BUILD_TEST)
 	add_subdirectory(${GRM_TEST_DIR}) # resman tests (only if project is not included as subproject)
 endif()
 
-if(GRM_GENERATE_MCSS_DOCS)
+if(GRM_GENERATE_DOCS)
 	include(${GRM_ROOT}/cmake/automatics/docs.cmake)
 endif()
