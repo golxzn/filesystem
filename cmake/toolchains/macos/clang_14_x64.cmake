f12353d960f7dd3ce1d7b96de354499df364b99b
Ruslan Golovinsky <ruslan.golovinsky@mytona.com>
2023-07-26 16:27:00 +0400

    revert changes

--- cmake/toolchains/macos/clang_14_x64.cmake
+++ cmake/toolchains/macos/clang_14_x64.cmake
@@ -4,10 +4,9 @@ set(CMAKE_SYSTEM_PROCESSOR x86_64 CACHE STRING "System processor")
 set(CMAKE_HOST_SYSTEM_PROCESSOR x86_64 CACHE STRING "Host system processor")
 
 set(CMAKE_C_COMPILER clang)
-set(CMAKE_C_COMPILER_VERSION 14)
 set(CMAKE_CXX_COMPILER clang++)
+set(CMAKE_C_COMPILER_VERSION 14)
 set(CMAKE_CXX_COMPILER_VERSION 14)
-set(CMAKE_CXX_FLAGS_INIT -fexperimental-library)
 
 find_program(ccache_program ccache)
 if (NOT ${ccache_program} MATCHES "NOTFOUND")
