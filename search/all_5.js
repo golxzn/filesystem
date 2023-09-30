c965630d0951ae33e8edddc98b52178477716569
golxzn <golxzn@users.noreply.github.com>
2023-09-30 07:11:00 +0000

    deploy: 1a1fad11bb563db1eb76d769aea7d48157c5a792

--- search/all_5.js
+++ search/all_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['get_5fassociation_0',['get_association',['../classgolxzn_1_1os_1_1filesystem.html#a2d713d1f750a982d6556cdd8665f02ac',1,'golxzn::os::filesystem::get_association(const std::wstring_view protocol) noexcept'],['../group__aliases.html#gaf65678cca3ebcc91ddb7e87e2bc5dae1',1,'golxzn::os::filesystem::get_association(const std::string_view protocol) noexcept']]],
-  ['golxzn_3a_3agolxzn_3a_3aos_3a_3afilesystem_1',['golxzn::golxzn::os::filesystem',['../index.html',1,'']]]
+  ['golxzn_3a_3aos_3a_3afilesystem_1',['golxzn::os::filesystem',['../index.html',1,'']]]
 ];
